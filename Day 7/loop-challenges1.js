5e4d5363894b055223ad57a864604c5695d6ad67
Himanshu Mishra <himanshu.2010m@gmail.com>
2025-07-19 17:29:02 +0530

    Day 6: Solved array challenges in JavaScript

---
+++ Day 7/loop-challenges1.js
@@ -0,0 +1,11 @@
+// Write a while loop that adds the numbers 4 and 5 together and stores the result in a variable called sum.
+let i = 0;
+let sum = 0;
+
+while (i <=5) {
+    sum += i;
+    i++;
+}
+
+console.log(sum);
+
---
+++ Day 7/loop-challenges2.js
@@ -0,0 +1,9 @@
+let i = 1;
+let count = 5;
+let countdown = []
+while (count >= i) {
+    countdown.push(i);
+    i++;
+}
+
+console.log(countdown);
