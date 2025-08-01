1180dd5bdb55ebe54d46eaca5f6168ca98c4e7c0
Himanshu Mishra <himanshu.2010m@gmail.com>
2025-08-01 20:42:25 +0530

    Day 19 - Advance JS concepts

---
+++ Day 19/advance1.js
@@ -0,0 +1,15 @@
+// Asynchronous
+console.log("Chai code");
+
+function sayHello() {
+    console.log("I would like to  say hello!");
+}
+
+setTimeout(() => {
+    sayHello();
+}, 4000);
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);  
+}
+
