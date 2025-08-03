bc2a2950c37f5533b2f12e87154acb044e37d5d6
Himanshu Mishra <himanshu.2010m@gmail.com>
2025-08-03 21:59:59 +0530

    Day 21: async-await, iterator-generator, ES6 module & CommomJS

---
+++ Day 21/appC.js
@@ -0,0 +1,2 @@
+const  mathOperation = require("./mathsOperationC.js");
+console.log(mathOperation.add(1, 2));
---
+++ Day 21/appM.js
@@ -0,0 +1,9 @@
+// Default import 
+import multiply from "./mathsOperationM.js"
+
+// name 
+import {add, subtract} from "./mathsOperationM.js"
+
+console.log(multiply(2 , 4));
+console.log(add(2, 3));
+
